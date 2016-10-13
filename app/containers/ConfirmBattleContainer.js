67445126e28c80f8ad2029625e3fe8a29f856356
JoeChildress <joe.ch@comcast.net>
2016-10-13 15:41:10 -0700

    added beginning of results

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -8,46 +8,36 @@ var ConfirmBattleContainer = React.createClass({
   },
   getInitialState: function () {
     return {
-      isLoading : true,
-      playersInfo: []
+      isLoading: true,
+      playersInfo: [],
     }
   },
   componentDidMount: function () {
-    
-    //get query values
     var query = this.props.location.query;
     githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
-    
-    //previous method returns a promise we can add 
-    .then(function (players) {
-      this.setState({
-        isLoading: false,
-        playersInfo: [ players[0], players[1] ]
-      
-      });
-      
-      //passing in this to inner function to retain this is same as outer 
-    }.bind(this))
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
   handleInitiateBattle: function () {
     this.context.router.push({
       pathname: '/results',
-      
-      //router allows pushing state through to this new route
       state: {
-        playerInfo: this.state.playersInfo
+        playersInfo: this.state.playersInfo
       }
-    });
+    })
   },
   render: function () {
     return (
-      <ConfirmBattle 
+      <ConfirmBattle
         isLoading={this.state.isLoading}
         onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} />
-    );
+    )
   }
-      
 });
-    
+
 module.exports = ConfirmBattleContainer;
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,33 +1,59 @@
 var axios = require('axios');
 
-var id = 'YOUR_CLIENT_ID';
-var sec = 'YOUR_SECRET_ID';
-var param = '?client_id=' + id + '&client_secret' + sec;
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUsersInfo (username) {
-  
-  //returns a promise
-  return axios.get('https://api.github.com/users/' + username + param)
+function getUserInfo (username) {
+  return axios.get('https://api.github.com/users/' + username + param);
+}
+
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData (player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores (players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
 }
 
 var helpers = {
   getPlayersInfo: function (players) {
-    //loop over every username in player array using .map
-    //axios.all takes ar of promises
-    //axios.all returns a promise where it is being called
     return axios.all(players.map(function (username) {
-      
-      //returns a promise
-      return getUsersInfo(username)
-    })).then(function (info) {
-      
-      //returns array of only the data property for each user
+      return getUserInfo(username)
+    }))
+    .then(function (info) {
       return info.map(function (user) {
-        return user.data;
+        return user.data
       })
-    }).catch(function (err) {
-      console.warn('Error in getPlayersInfo', err);
     })
+    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
+  },
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
   }
 };
 
